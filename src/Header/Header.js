fb4072e8a64568c86d4092d53c4a2432e03cc8d5
TOTA JEEVAN SAGAR <128782896+JeevansagarTota@users.noreply.github.com>
2023-03-28 22:55:57 +0530

    header changes

---
+++ src/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.css'
+
+export  const Header = () => {
+  return (
+    <div className='header text-center bg-primary text-white'>My First React Application</div>
+  )
+}
+
+
